8e5d3f7ed14addd37c0806dfd98074dc49dfcb3f
William Babbitt <babbittw@umich.edu>
2025-01-12 18:06:53 -0500

    Create drivetrain subsystem and drive command in robotContainer
    We create a factory function that creates a drive command for our drivetrain. We then use this factory function to create a command as the default for the drivetrain subsystem, which means it will run all the time.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,25 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.Drive;
+import frc.robot.subsystems.Drivetrain;
 
 public class RobotContainer {
+  private final Drivetrain m_Drivetrain = new Drivetrain();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    m_Drivetrain.setDefaultCommand(getArcadeDriveCommand());
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
+  }
+
+  public Command getArcadeDriveCommand() {
+    return new Drive(m_Drivetrain, () -> 0, () -> 0);
   }
 }
