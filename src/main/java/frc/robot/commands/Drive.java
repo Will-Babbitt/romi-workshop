a0c642472177bd5a9f200180a5cecda5aab3df5c
William Babbitt <babbittw@umich.edu>
2025-01-12 17:57:36 -0500

    Add drivetrain to drive command as requirement
    This creates a local drivetrain to reference in the command and also informs the robot container that this command requires the drivetrain subsystem to run.

--- src/main/java/frc/robot/commands/Drive.java
+++ src/main/java/frc/robot/commands/Drive.java
@@ -1,10 +1,14 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Drivetrain;
 
 public class Drive extends Command {
-
-  public Drive() {}
+    private final Drivetrain m_Drivetrain;
+    public Drive(Drivetrain drivetrain) {
+        m_Drivetrain = drivetrain;
+        addRequirements(drivetrain);
+    }
 
   // Called when the command is initially scheduled.
   @Override
@@ -12,7 +16,9 @@ public class Drive extends Command {
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    m_Drivetrain.arcadeDrive(0, 0);
+  }
 
   // Called once the command ends or is interrupted.
   @Override
