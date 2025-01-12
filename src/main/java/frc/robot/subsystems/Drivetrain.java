2ffca48e04335ad69ebcb00a736ae96333954934
William Babbitt <babbittw@umich.edu>
2025-01-12 17:44:45 -0500

    create differentialDrive and make arcadeDrive function
    The differentialDrive is provided from WPI and can be passed in speed values. We make a public function here to call later in our drive command.

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -9,7 +10,13 @@ public class Drivetrain extends SubsystemBase {
     private final Spark m_leftMotor = new Spark(0);
     private final Spark m_rightMotor = new Spark(1);
 
+    private final DifferentialDrive m_Drive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
     public Drivetrain() {
         m_rightMotor.setInverted(true);
+    }
+
+    public void arcadeDrive(double xSpeed, double zRotation) {
+        m_Drive.arcadeDrive(xSpeed, zRotation);
     }
 }
